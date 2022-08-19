10113ae08b9bcbf5340088d7ab306ee591461e05
anapeksha <anapeksha.mukherjee@gmail.com>
2022-08-19 16:02:57 +0530

    Box styles added

---
+++ src/styles/CustomBox.style.js
@@ -0,0 +1,10 @@
+import { Box, styled } from "@mui/material";
+
+const CustomBox = styled(Box)({
+	borderRadius: "7px",
+	borderWidth: "2px",
+	padding: "2vw",
+	backgroundImage: "linear-gradient(#4e27b6,#7927c8)",
+	boxShadow: "rgb(21 77 134) 0px 10px 70px 10px",
+});
+export default CustomBox;
