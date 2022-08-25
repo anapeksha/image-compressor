0c69c6e3a4cd20ac5b91a4c849ef1391e0b41f52
anapeksha <anapeksha.mukherjee@gmail.com>
2022-08-26 02:49:37 +0530

    made different buttons, set to make unique name

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,3 +1,4 @@
+import AutorenewIcon from "@mui/icons-material/Autorenew";
 import DownloadIcon from "@mui/icons-material/Download";
 import FileUploadRoundedIcon from "@mui/icons-material/FileUploadRounded";
 import {
@@ -10,12 +11,55 @@ import {
 	InputLabel,
 } from "@mui/material";
 import React from "react";
+import handleCompression from "../controllers/handleCompression";
 import handleSize from "../controllers/handleSize";
-import handleUpload from "../controllers/handleUpload";
+import setUniqueFileName from "../controllers/setUniqueFileName";
 import { CustomBox, CustomButton, Wrapper } from "../styles";
 import Dropdown from "./Dropdown";
+import Progress from "./Progress";
 
 function Form(props) {
+	const handleButtonDisplay = () => {
+		if (props.image !== undefined && props.convertedImage === undefined) {
+			return (
+				<Button
+					variant="text"
+					size="small"
+					color="error"
+					endIcon={<AutorenewIcon />}
+					onClick={() => {
+						var sizeinMB = handleSize(props.size, props.sizeUnit);
+						handleCompression(
+							props.image,
+							sizeinMB,
+							props.setProgress,
+							props.setConvertedImage
+						);
+					}}
+				>
+					Convert
+				</Button>
+			);
+		} else if (
+			props.image !== undefined &&
+			props.convertedImage !== undefined
+		) {
+			return (
+				<Button
+					variant="outlined"
+					component="a"
+					size="small"
+					color="success"
+					endIcon={<DownloadIcon />}
+					download={setUniqueFileName()}
+					href={props.convertedImage}
+				>
+					Download
+				</Button>
+			);
+		}
+	};
+
 	return (
 		<Wrapper>
 			<CustomBox>
@@ -58,25 +102,19 @@ function Form(props) {
 										hidden
 										accept="image/*"
 										type="file"
-										onChange={(e) => {
-											var sizeinMB = handleSize(props.size, props.sizeUnit);
-											handleUpload(e, sizeinMB, props.setImage);
+										onChange={(event) => {
+											props.setImage(event.target.files[0]);
 										}}
 									/>
 								</CustomButton>
 							</Box>
-							<Button
-								variant="text"
-								component="a"
-								endIcon={<DownloadIcon />}
-								download
-								href={props.image}
-							>
-								Download
-							</Button>
+							<Box style={{ display: "flex", justifyContent: "center" }}>
+								{handleButtonDisplay()}
+							</Box>
 						</form>
 					</FormControl>
 				</FormGroup>
+				<Progress progress={props.progress} />
 			</CustomBox>
 		</Wrapper>
 	);
