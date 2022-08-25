7631d3eee1a42e996d42e4fea1088093dac396e0
anapeksha <anapeksha.mukherjee@gmail.com>
2022-08-25 10:25:26 +0530

    component ready

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,45 +1,79 @@
+import DownloadIcon from "@mui/icons-material/Download";
 import FileUploadRoundedIcon from "@mui/icons-material/FileUploadRounded";
 import {
+	Box,
 	Button,
 	FormControl,
 	FormGroup,
 	Input,
 	InputAdornment,
 	InputLabel,
-	Stack,
 } from "@mui/material";
 import React from "react";
-import CustomBox from "../styles/CustomBox.style";
-import Wrapper from "../styles/Wrapper";
+import handleSize from "../controllers/handleSize";
+import handleUpload from "../controllers/handleUpload";
+import { CustomBox, CustomButton, Wrapper } from "../styles";
 import Dropdown from "./Dropdown";
 
-function Form() {
+function Form(props) {
 	return (
 		<Wrapper>
 			<CustomBox>
 				<FormGroup>
 					<FormControl>
 						<form>
-							<Stack spacing={2}>
-								<InputLabel htmlFor="size-label">Size</InputLabel>
+							<Box>
+								<InputLabel htmlFor="size-label" style={{ color: "white" }}>
+									Size
+								</InputLabel>
 								<Input
 									endAdornment={
 										<InputAdornment position="end">
-											<Dropdown />
+											<Dropdown setSizeUnit={props.setSizeUnit} />
 										</InputAdornment>
 									}
+									style={{ color: "white", borderColor: "white" }}
+									value={props.size}
+									autoFocus
+									onChange={(e) => {
+										props.setSize(e.target.value);
+									}}
 								/>
-								<Button
-									color="primary"
+							</Box>
+							<Box
+								style={{
+									display: "flex",
+									justifyContent: "center",
+									padding: "8px",
+								}}
+							>
+								<CustomButton
 									aria-label="upload photo"
 									size="small"
 									component="label"
 									endIcon={<FileUploadRoundedIcon />}
 								>
 									Upload
-									<input hidden accept="image/*" type="file" />
-								</Button>
-							</Stack>
+									<input
+										hidden
+										accept="image/*"
+										type="file"
+										onChange={(e) => {
+											var sizeinMB = handleSize(props.size, props.sizeUnit);
+											handleUpload(e, sizeinMB, props.setImage);
+										}}
+									/>
+								</CustomButton>
+							</Box>
+							<Button
+								variant="text"
+								component="a"
+								endIcon={<DownloadIcon />}
+								download
+								href={props.image}
+							>
+								Download
+							</Button>
 						</form>
 					</FormControl>
 				</FormGroup>
