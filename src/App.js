7cd8882b24c1771aa334282a216dc3b0742e6f78
anapeksha <anapeksha.mukherjee@gmail.com>
2022-08-25 10:23:40 +0530

    Form and Progress set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Form from "./components/Form";
+import Progress from "./components/Progress";
 
 function App() {
+	const [size, setSize] = useState(0);
+	const [sizeUnit, setSizeUnit] = useState("KB");
+	const [image, setImage] = useState(undefined);
+	const [progress, setProgress] = useState(0);
+
+	useEffect(() => {
+		console.log(size, sizeUnit, image);
+	}, [size, sizeUnit, image]);
+
 	return (
 		<div>
-			<Form />
+			<Form
+				size={size}
+				sizeUnit={sizeUnit}
+				image={image}
+				setSize={setSize}
+				setSizeUnit={setSizeUnit}
+				setImage={setImage}
+				setProgress={setProgress}
+			/>
+			<Progress progress={progress}/>
 		</div>
 	);
 }
