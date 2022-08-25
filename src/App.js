4ae6242189989d9f661e3806bcb305debe0473bf
anapeksha <anapeksha.mukherjee@gmail.com>
2022-08-26 02:48:58 +0530

    Progress working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from "react";
 import Form from "./components/Form";
-import Progress from "./components/Progress";
 
 function App() {
-	const [size, setSize] = useState(0);
+	const [size, setSize] = useState(undefined);
 	const [sizeUnit, setSizeUnit] = useState("KB");
 	const [image, setImage] = useState(undefined);
+	const [convertedImage, setConvertedImage] = useState(undefined);
 	const [progress, setProgress] = useState(0);
 
 	useEffect(() => {
 		console.log(size, sizeUnit, image);
-	}, [size, sizeUnit, image]);
+	}, [size, sizeUnit, image, progress]);
 
 	return (
 		<div>
@@ -18,12 +18,14 @@ function App() {
 				size={size}
 				sizeUnit={sizeUnit}
 				image={image}
+				convertedImage={convertedImage}
+				progress={progress}
 				setSize={setSize}
 				setSizeUnit={setSizeUnit}
 				setImage={setImage}
+				setConvertedImage={setConvertedImage}
 				setProgress={setProgress}
 			/>
-			<Progress progress={progress}/>
 		</div>
 	);
 }
--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,9 +1,30 @@
-import React from 'react';
-import {CircularProgress} from "@mui/material";
-import handleProgress from '../controllers/handleProgress';
+import { Backdrop, CircularProgress } from "@mui/material";
+import React, { useEffect, useState } from "react";
 
 function Progress(props) {
-  return <CircularProgress variant="determinate" value={props.handleProgress} />;
+	const [open, setOpen] = useState(false);
+
+	useEffect(() => {
+		handleOpen();
+	}, [props.progress]);
+
+	const handleOpen = () => {
+		if (props.progress > 0) {
+			setOpen(true);
+		}
+		if (props.progress === 100) {
+			setOpen(false);
+		}
+	};
+
+	return (
+		<Backdrop
+			sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+			open={open}
+		>
+			<CircularProgress variant="determinate" value={props.progress} />
+		</Backdrop>
+	);
 }
 
-export default Progress
+export default Progress;
