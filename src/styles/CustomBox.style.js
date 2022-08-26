de5a6d2c511145637a14ff919b901a41f73e0681
anapeksha <anapeksha.mukherjee@gmail.com>
2022-08-26 09:29:15 +0530

    padding changed

--- src/styles/CustomBox.style.js
+++ src/styles/CustomBox.style.js
@@ -3,7 +3,7 @@ import { Box, styled } from "@mui/material";
 const CustomBox = styled(Box)({
 	borderRadius: "7px",
 	borderWidth: "2px",
-	padding: "2vw",
+	padding: "5vh 5vw",
 	backgroundImage: "linear-gradient(#4e27b6,#7927c8)",
 	boxShadow: "rgb(21 77 134) 0px 10px 70px 10px",
 });
