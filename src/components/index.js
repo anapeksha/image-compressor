ee412d0bc0a5e80659c71fdd581fc22e55ae6361
anapeksha <anapeksha.mukherjee@gmail.com>
2022-08-26 10:23:17 +0530

    easier export import

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Dropdown from "./Dropdown";
 import Form from "./Form";
+import Navbar from "./Navbar";
 import Progress from "./Progress";
 
-export { Dropdown, Form, Progress };
+export { Dropdown, Form, Progress, Navbar };
